da68e71126fc7e0162f3829850a857eebf68c212
Rick-torrellas <ricardo.torrejas@gmail.com>
2026-02-25 12:28:44 -0400

    fix: reparar las dependencias del init

--- src/core_cpu_monitor/__init__.py
+++ src/core_cpu_monitor/__init__.py
@@ -1,5 +1,3 @@
 from core_cpu_monitor.domain import CPUStatus, CPUStateCheck
-from core_cpu_monitor.monitor import CPUMonitor
-from core_cpu_monitor.ports import Logger
 
-__all__ = ["CPUStatus", "CPUMonitor", "Logger", "CPUStateCheck"]
+__all__ = ["CPUStatus", "CPUMonitor", "CPUStateCheck"]
